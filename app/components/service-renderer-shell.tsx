d81f0d227e30cb4c2b4a474d6f17ad95428a2d73
kzoeps <kzoepa@gmail.com>
2025-07-25 22:37:38 +0600

    feat: add service renderer and view original

--- app/components/service-renderer-shell.tsx
+++ app/components/service-renderer-shell.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import dynamic from "next/dynamic";
+import { useState } from "react";
 
 const ServiceRenderer = dynamic(
     () => import("@/utils/service-parser").then((mod) => mod.ServiceRenderer),
@@ -15,10 +17,26 @@ export default function ServiceRendererShell({
     serviceName: string;
     htmlContent: string;
 }) {
+    const [showOriginal, setShowOriginal] = useState(false);
+    const toggleOriginal = () => {
+        setShowOriginal(!showOriginal);
+    };
     return (
-        <ServiceRenderer
-            serviceName={serviceName || ""}
-            htmlContent={htmlContent || ""}
-        />
+        <div className="flex flex-col">
+            <Button onClick={toggleOriginal} className="mx-6 mt-6">
+                {!showOriginal ? "View Original" : "Beautify"}{" "}
+            </Button>
+            {!showOriginal && (
+                <ServiceRenderer
+                    serviceName={serviceName || ""}
+                    htmlContent={htmlContent || ""}
+                />
+            )}
+            <div className="p-6">
+                {showOriginal && (
+                    <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+                )}
+            </div>
+        </div>
     );
 }
